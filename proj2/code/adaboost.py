3b5440c0fe82d4e666cffa74dd91e75f482919f3
Jerry Bonnell <j.bonnell@miami.edu>
2017-11-12 02:54:11 -0500

    implement perceptron

---
+++ proj2/code/adaboost.py
@@ -0,0 +1,123 @@
+#
+# adaboost.py
+# 
+# date last modified: 12 nov 2017
+# modified last by: jerry
+# 
+#
+
+import math
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+from random import randint
+from sklearn.neighbors import KNeighborsClassifier
+#from matplotlib.colors import ListedColormap
+from perceptron import PerceptronClassifier
+
+# once again, change this to switch datasets 
+FILENAME = "dataset/default.csv" 
+# probability of an example being in the training set 
+PROBABILITY_TRAINING_SET = 0.5
+
+# learning rate for perceptron 
+ETA = 0.5 
+# desired threshold for error rate; 0.2 --> 20% 
+THRESHOLD = 0.2
+# maximum number of epochs for training
+UPPER_BOUND = 200 
+# verbose flag 
+IS_VERBOSE = True 
+
+def split_dataset(examples, prob_training):
+	"""
+	receives list of examples  
+	returns a tuple consisting of a training set and testing set 
+	"""
+	training_set = []
+	testing_set = []
+
+	# generate a random number [1,100]. if it is greater than 
+	# prob_training then add the example to the 
+	# testing set; otherwise add it to the training set 
+	percent_training = prob_training * 100; 
+	for example in examples:
+		result = randint(1, 100) 
+		# if the result is a number less than percent_training, 
+		# add to training set; else add it to the testing set 
+		if (result < percent_training):
+			training_set.append(example)
+		else:
+			testing_set.append(example)
+
+	return (training_set, testing_set)
+
+def load_dataset(filename):
+	"""
+	given a filename that points to a file containing the data-set, 
+	load it into memory and return an array containing this data-set
+	"""
+	dataset = []
+	# open the data-set file
+	file = open(filename, "r")
+	# we want to load this data-set into a 2D array 
+	# where each row is an example and each column is 
+	# an attribute. 
+	for line in file: 
+		example = line.strip().split(",") # a row in the data-set 
+		dataset.append(list(map(float, example[1:]))) # append it to the 2D array
+
+	return dataset 
+
+def split_attribute_and_label(dataset):
+	"""
+	split attribute vectors from their class-labels 
+	"""
+
+	# add 0.1 because values are processed as floats and we may have 0.999...
+	class_labels = [int(row[-1] + 0.1) for row in dataset]
+	attributes = [row[0:-1] for row in dataset]
+
+	return (attributes, class_labels)
+
+def calculate_error(class_labels, hypothesis_list):
+	"""
+	calculates simple error rate on a dataset
+	:param class_labels: list of given class-labels 
+	:param hypothesis_list: list of classifier predictions for examples
+	"""
+	num_errors = 0
+	for i in range(len(class_labels)):
+		if class_labels[i] != hypothesis_list[i]:
+			num_errors += 1
+
+	return (num_errors / len(class_labels))
+
+# preprocessing: load in the dataset and split into a training and testing set 
+dataset = load_dataset(FILENAME) 
+(training_set,testing_set) = split_dataset(dataset, PROBABILITY_TRAINING_SET)
+print("training set size: %s testing set size: %s num instances: %s" % 
+	(len(training_set), len(testing_set), len(dataset)))
+
+# because datasets sometimes place the class attribute at the end or even 
+# at the beginning or the middle, we'll separate the attribute vector from
+# the class-label. also note that this is the way scikit-learn does it. 
+# train_x: the attribute vector; train_y: the class_label  
+(train_x, train_y) = split_attribute_and_label(training_set)
+(test_x, test_y) = split_attribute_and_label(testing_set)
+
+# create the perceptron classifier 
+linear_classifier = PerceptronClassifier(ETA, THRESHOLD, UPPER_BOUND, IS_VERBOSE)
+# train the classifier 
+linear_classifier.fit(train_x, train_y)
+print("Training error rate %s" % linear_classifier.training_error_rate)
+print(linear_classifier.weights)
+
+# test the trained classifier on the testing set 
+result_list = linear_classifier.predict(test_x)
+print("Testing error rate %s" % calculate_error(test_y, result_list))
+
+
+
+
---
+++ proj2/code/perceptron.py
@@ -0,0 +1,185 @@
+#
+# perceptron.py
+# 
+# date last modified: 12 nov 2017
+# modified last by: jerry
+# 
+# implementation of the linear classifier, perceptron
+#
+
+from random import random
+from optparse import OptionParser
+
+class PerceptronClassifier:
+
+	def __init__(self, eta, threshold, upper_bound, verbose = False):
+		"""
+		constructor 
+		"""
+		# member variables for this class 
+		self.learning_rate = eta
+		self.threshold = threshold
+		self.upper_bound = upper_bound
+		self.verbose = verbose
+
+		self.weights = []
+		self.training_error_rate = 0
+		self.training_labels = []
+		self.normal_train_data = []
+
+	def __normalize(self, data_set):
+		"""
+		normalize the dataset with values [0,1]
+		"""
+		normal = []
+		min_tuple = data_set[0][:]
+		max_tuple = data_set[0][:]
+		# find the min and max tuple
+		for example in data_set:
+			for i in range(len(example)):
+				if example[i] < min_tuple[i]:
+					# print("i ex min", i, example[i], min_tuple[i])
+					min_tuple[i] = example[i]
+				if example[i] > max_tuple[i]:
+					max_tuple[i] = example[i]
+
+		for example in data_set:
+			tmp = []
+			#print(example)
+			for i in range(len(example)):
+				#print(i)
+				tmp.append((example[i] - min_tuple[i])/ (max_tuple[i] - min_tuple[i]))
+			#tmp.append(example[-1])
+			normal.append(tmp)  # append the modified tuple to normalized array
+		return normal
+
+
+	def __calculate_evidence(self, weights, example):
+		"""
+		calculates the evidence of sum(wi*xi), which is all the weights
+		multiplied by the example attributes
+		"""
+
+		# sum up the wi*xi, we'll assume w0 is at end of the vector
+		evidence = 0
+		for i in range(len(weights) - 1):
+			evidence += weights[i] * example[i]
+		evidence += weights[-1]  # -1 is the w0 x0 = 1
+
+		return evidence
+
+	def __calculate_training_error(self):
+		"""
+		calculates the simple error rate on the training set 
+		"""
+		num_errors = 0
+		for i in range(len(self.normal_train_data)):
+			example = self.normal_train_data[i][:]
+			hypothesis = 1 if self.__calculate_evidence(self.weights, example) > 0 else 0
+			if self.training_labels[i] != hypothesis:
+				num_errors += 1.0
+
+		return (num_errors / len(self.training_labels))
+
+
+	def fit(self, attribute_vectors, training_labels):
+		"""
+		'fits' the classifier given a training set and a set of class labels 
+		for that training set 
+		:param attribute_vectors: list of attribute vectors 
+		:param training_labels: list of class labels associated with examples
+		"""
+
+		# first normalize the attribute vectors 
+		self.normal_train_data = self.__normalize(attribute_vectors)
+		self.training_labels = training_labels[:]
+
+		# keep track of the best linear equation, in case threshold is 
+		# never reached 
+		best_weights = []
+		best_error_rate  = 1
+
+		#
+		# adapted perceptron-learning algorithm from the textbook... 
+		#
+		# TABLE 4.1 (pg. 71)
+		# 1. Initialize all weights, wi, to small random numbers. 
+		#    Choose an appropriate learning rate, eta, between 0 and 1. 
+		# 2. For each training example, whose class is c(x) 
+		#    (i) Let h(x) = 1 if evidence > 0 and h(x) = 0 otherwise 
+		#    (ii) Update each weight using the formula: 
+		#         wi = wi + eta * [c(x) - h(x)] * xi 
+		# 3. If c(x) = h(x) for all training examples, stop; otherwise 
+		#    return to 2. 
+		# 
+
+		# equation has form: w0 + w1 * x1 + ... + wn * xn = 0 
+		# STEP 1: initialize the weights for all wi, to small random numbers
+		for i in range(len(self.normal_train_data[0])):
+			self.weights.append(random()/3)  # i'th weight
+
+		# now put the w0 weight at the end of the equation (list) 
+		# usually its the first weight but to make our implementation
+		# easier, we put it at the end
+		self.weights.append(random()/3)
+
+
+		epoch = 1
+		error_rate = 100
+		# STEP 2: for each training example... 
+		while True:
+			# calculate the evidence
+			for k in range(len(self.normal_train_data)):
+				example = self.normal_train_data[k][:]
+				# (i) determine the hypothesis, h
+				hypothesis = 1 if self.__calculate_evidence(self.weights, example) > 0 else 0
+				# (ii) update each weight according to the formula
+				#  delta_weight = eta * [c(x) - h(x)]
+				delta_weight = self.learning_rate * (self.training_labels[k] - hypothesis)
+				for i in range(len(self.weights) - 1):
+					# wi = wi + delta_weight * xi 
+					self.weights[i] += delta_weight * example[i]
+				# remember to modify the bias, w0 
+				self.weights[-1] += delta_weight
+
+			# STEP 3: does c(x) = h(x) for all training examples? or does it 
+			# meet a threshold? 
+			error_rate = self.__calculate_training_error()
+			if epoch % 5 == 0 and self.verbose:
+				print("--> err4epoch: {:f} e: {:d}".format(error_rate, epoch))
+
+			# meets the threshold error rate; training ends 
+			if error_rate < self.threshold:
+				self.training_error_rate = error_rate
+				break
+			# otherwise if this equation is the best seen, save it 
+			elif error_rate < best_error_rate:
+				best_error_rate = error_rate
+				best_weights = self.weights[:]
+			
+			# c(x) != h(x) for all examples and did not meet the threshold
+			# return to STEP 2 if upper-bound limit has not exceeded.  
+			epoch += 1
+
+			# now check if we have crossed the upper bound
+			if epoch >= self.upper_bound :
+				self.weights = best_weights[:]
+				self.training_error_rate = best_error_rate
+				break
+
+	def predict(self, testing_set):
+		"""
+		given a trained linear classifier, predict the classes 
+		of the testing set 
+
+		:return: list of hypotheses for each example in the testing set
+		"""
+		result_list = []
+		for k in range(len(testing_set)):
+			example = testing_set[k][:]
+			# determine the hypothesis, h
+			hypothesis = 1 if self.__calculate_evidence(self.weights, example) > 0 else 0
+			result_list.append(int(hypothesis))
+		return result_list
+ 
+
